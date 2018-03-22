a045f5f284b51a40603e09886f732718f087d86e
Aung Mon Chit Htwe <aungmonchithtwe@aungs-macbook-pro.local>
2018-03-23 00:36:28 +0800

    add 2 new funcions

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -20,6 +20,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        //
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        //
+    }
 
 }
 
